25b63c8ed47369817f3a2d869c850d36b9487f8d
Sebastian Huch <sebastian.huch@tum.de>
2022-06-24 18:21:04 +0200

    restructure create_ImageSets -> train+val+TEST

--- tools/pretrain_utils/create_ImageSets.py
+++ tools/pretrain_utils/create_ImageSets.py
@@ -7,30 +7,40 @@ try:
     dir = sys.argv[1]
 except IndexError:
     dir = "/mnt/13_Vegas_Challenge/03_Data/02_Real/00_extracted_data/for_training/sim_noise_002/training/label_2"
-dist = 0.8
+split_train_valtest = 2/3
+split_val_test = 1/2
 step = 5
 
-initial_count = min(35000, len(os.listdir(dir)))
-print(f"Total files {initial_count}")
+total_files = min(30000, len(os.listdir(dir)))
+print(f"Total files {total_files}")
 
 # Distribution of the data
-train_size = dist * initial_count
-train_size = int(train_size)
-print(f"Undownsampled training size (80% of total size) {train_size}")
-print(f"Undownsampled validation size (20% of total size) {initial_count - train_size}")
+train_size = int(split_train_valtest * total_files)
+val_size = int((1 - split_train_valtest) * split_val_test * total_files)
+test_size = int((1 - split_train_valtest) * (1 - split_val_test) * total_files)
+print(f"Undownsampled training size ({train_size / total_files * 100} %  of total size): {train_size}")
+print(f"Undownsampled validation size ({val_size / total_files * 100} % of total size): {val_size}")
+print(f"Undownsampled testing size ({test_size / total_files * 100} % of total size): {test_size}")
 
 train_list = [str(idx).zfill(6) for idx in np.arange(0, train_size, step)]
-validation_list = [str(idx).zfill(6) for idx in np.arange(int(train_list[-1]) + step, initial_count, step)]
+validation_list = [str(idx).zfill(6) for idx in np.arange(int(train_list[-1]) + step, train_size + val_size, step)]
+testing_list = [str(idx).zfill(6) for idx in np.arange(int(validation_list[-1]) + step, total_files, step)]
 
 print(f"Number of train samples (downsampled by factor {step}): {len(train_list)}")
 print(f"Number of validation samples (downsampled by factor {step}): {len(validation_list)}")
+print(f"Number of testing samples (downsampled by factor {step}): {len(testing_list)}")
 
-#Training
+# Training
 with open('train.txt', 'w') as temp_file:
     for item in train_list:
         temp_file.write("%s\n" % item)
 
-#Validation
+# Validation
 with open('val.txt', 'w') as temp_file:
     for item in validation_list:
+        temp_file.write("%s\n" % item)
+
+# Testing
+with open('test.txt', 'w') as temp_file:
+    for item in testing_list:
         temp_file.write("%s\n" % item)
