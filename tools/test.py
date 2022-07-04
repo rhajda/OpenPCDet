a726f523349eae156529847786bac3dd6d1adff1
Sebastian Huch <sebastian.huch@tum.de>
2022-07-04 10:50:04 +0200

    set random seed in testing

--- tools/test.py
+++ tools/test.py
@@ -13,6 +13,21 @@ import numpy as np
 import torch
 from tensorboardX import SummaryWriter
 
+
+def set_random_seed(seed):
+    # set fixed determinism seed
+    import random
+    import numpy as np
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+set_random_seed(777)
+
 from eval_utils import eval_utils
 from pcdet.config import cfg, cfg_from_list, cfg_from_yaml_file, log_config_to_file
 from pcdet.datasets import build_dataloader
