5cebcb7973ce8540abf95934f39600fb9399fa95
Sebastian Huch <sebastian.huch@tum.de>
2022-07-04 13:22:43 +0200

    bugfix RNG in dataloader

--- pcdet/datasets/__init__.py
+++ pcdet/datasets/__init__.py
@@ -89,7 +89,7 @@ def build_dataloader(dataset_cfg, class_names, batch_size, dist, root_path=None,
     dataloader = DataLoader(
         dataset, batch_size=batch_size, pin_memory=True, num_workers=workers,
         shuffle=(sampler is None) and training, collate_fn=dataset.collate_batch,
-        drop_last=False, sampler=sampler, timeout=0, worker_init_fn=seed_worker,
+        drop_last=False, sampler=sampler, timeout=0, worker_init_fn=seed_worker, generator=g
     )
 
     return dataset, dataloader, sampler
