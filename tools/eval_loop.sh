84ccee4a26a759433df3f2338f2feecf9c37e54b
Sebastian Huch <sebastian.huch@tum.de>
2022-09-16 13:27:14 -0400

    corrected comments in eval_loop

--- tools/eval_loop.sh
+++ tools/eval_loop.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# DATASET = test dataset
+# DATASET = train dataset
 for DATASET in default_real_run sim_no_noise sim_noise002 sim_noise_down
 do
     # i = 5 training repetitions for each training dataset
     for i in {1..5}
         do
                 # choose pointrcnn or pointpillar for cfg_file and ckpt_dir
-                # eval_tag = training dataset (same as mounted dataset in docker)
+                # eval_tag = test dataset (same as mounted dataset in docker)
                 python test.py --cfg_file cfgs/indy_models/pointrcnn.yaml --extra_tag "${DATASET}_${i}" --ckpt_dir /home/output/indy_models/pointrcnn/${DATASET}_${i}/ckpt/ --eval_all --start_epoch 46 --eval_tag sim_noise_down --max_waiting_mins 0
         done
 done
