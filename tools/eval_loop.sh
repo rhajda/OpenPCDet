cbb924975038dca35ae2488d7978fc017ae16203
Sebastian Huch <sebastian.huch@tum.de>
2022-10-05 16:38:44 +0200

    bugfix

--- tools/eval_loop.sh
+++ tools/eval_loop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-NETWORK = pointrcnn # pointrcnn or pointpillar
-EVAL_TAG = sim_no_noise # real or sim_no_noise or sim_noise002 or sim_noise_down
+NETWORK=pointrcnn # pointrcnn or pointpillar
+EVAL_TAG=sim_no_noise # real or sim_no_noise or sim_noise002 or sim_noise_down
                         # EVAL_TAG = test dataset (same as mounted dataset in docker)
 
 # TRAIN_DATASET = train dataset
@@ -10,6 +10,6 @@ do
     # i = 5 training repetitions for each training dataset
     for i in {1..5}
         do
-                python test.py --cfg_file cfgs/indy_models/{NETWORK}.yaml --extra_tag "${TRAIN_DATASET}_${i}" --ckpt_dir /home/output/indy_models/{NETWORK}/${TRAIN_DATASET}_${i}/ckpt/ --eval_all --start_epoch 46 --eval_tag {EVAL_TAG} --max_waiting_mins 0
+                python test.py --cfg_file cfgs/indy_models/${NETWORK}.yaml --extra_tag "${TRAIN_DATASET}_${i}" --ckpt_dir /home/output/indy_models/${NETWORK}/${TRAIN_DATASET}_${i}/ckpt/ --eval_all --start_epoch 46 --eval_tag ${EVAL_TAG} --max_waiting_mins 0
         done
 done
