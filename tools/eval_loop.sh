e1da6b087705ae1851ef643e8fc3a024ab8516ae
Sebastian Huch <sebastian.huch@tum.de>
2022-10-05 16:29:26 +0200

    clean up eval_loop

--- tools/eval_loop.sh
+++ tools/eval_loop.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# DATASET = train dataset
-for DATASET in default_real_run sim_no_noise sim_noise002 sim_noise_down
+NETWORK = pointrcnn # pointrcnn or pointpillar
+EVAL_TAG = sim_no_noise # real or sim_no_noise or sim_noise002 or sim_noise_down
+                        # EVAL_TAG = test dataset (same as mounted dataset in docker)
+
+# TRAIN_DATASET = train dataset
+for TRAIN_DATASET in default_real_run sim_no_noise sim_noise002 sim_noise_down
 do
     # i = 5 training repetitions for each training dataset
     for i in {1..5}
         do
-                # choose pointrcnn or pointpillar for cfg_file and ckpt_dir
-                # eval_tag = test dataset (same as mounted dataset in docker)
-                python test.py --cfg_file cfgs/indy_models/pointrcnn.yaml --extra_tag "${DATASET}_${i}" --ckpt_dir /home/output/indy_models/pointrcnn/${DATASET}_${i}/ckpt/ --eval_all --start_epoch 46 --eval_tag sim_noise_down --max_waiting_mins 0
+                python test.py --cfg_file cfgs/indy_models/{NETWORK}.yaml --extra_tag "${TRAIN_DATASET}_${i}" --ckpt_dir /home/output/indy_models/{NETWORK}/${TRAIN_DATASET}_${i}/ckpt/ --eval_all --start_epoch 46 --eval_tag {EVAL_TAG} --max_waiting_mins 0
         done
 done
