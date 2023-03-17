19247e280f985a96f71e4c6e1d088ee2d76b82ff
chris <ga46lig@mytum.de>
2023-03-17 10:25:45 +0100

    change path to mount folders

--- run_docker_s2r.sh
+++ run_docker_s2r.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 gpu=$1
-docker run --rm -it --gpus "device=$gpu" --network=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/.Xauthority --hostname $(hostname) -v /home/calcstudenten/salinas/data/for_training/real:/home/data/s2r -v /data/salinas/openpcdet_trainings/sim-to-real:/home/output openpcdet:sim2real bash
+docker run --rm -it --gpus "device=$gpu" --network=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/.Xauthority --hostname $(hostname) -v /home/calcstudenten/salinas-huch/data/for_training/real:/home/data/s2r -v /data/salinas-huch/openpcdet_trainings/sim-to-real:/home/output openpcdet:sim2real bash
 
