14429e1f1adbd822179fd061004bde8ac339b6d8
Sebastian Huch <sebastian.huch@tum.de>
2023-04-17 09:18:15 +0200

    bugfix SECOND forward return

--- pcdet/models/detectors/second_net.py
+++ pcdet/models/detectors/second_net.py
@@ -32,7 +32,7 @@ class SECONDNet(Detector3DTemplate):
             return ret_dict, tb_dict, disp_dict
         else:
             pred_dicts, recall_dicts = self.post_processing(batch_dict)
-            return pred_dicts, recall_dicts
+            return pred_dicts, recall_dicts, None
 
     def get_training_loss(self):
         disp_dict = {}
