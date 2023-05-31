fcdfdba49dc084df410cdb98b876e24d577b3e7b
LoicStratil <stratil.loic@gmail.com>
2023-05-31 06:23:33 +0000

    trigger predict_objects.py

--- autolabel_pipeline/predict_objects.sh
+++ autolabel_pipeline/predict_objects.sh
@@ -1,15 +1,10 @@
 
 #!/usr/bin/env bash
 
-'''
-Triggers predict_objects.py
-'''
-
-
 # Assign arguments to the variables
-CFG_FILE="/home/tools/cfgs/autolabel_models/pointrcnn.yaml"
-CKPT_DIR="/home/output/autolabel_models/pointrcnn/default/ckpt/"
-CKPT='checkpoint_epoch_74.pth'
+CFG_FILE="/home/tools/cfgs/autolabel_models/pointpillar.yaml"
+CKPT_DIR="/home/output/home/tools/cfgs/autolabel_models/pointpillar/default/ckpt/"
+CKPT='checkpoint_epoch_75.pth'
 
 # Run:
 python predict_objects.py --cfg_file "$CFG_FILE" --ckpt_dir "$CKPT_DIR" --ckpt ${CKPT}
