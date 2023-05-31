1d45e5355f308b9e5d3f71185c6a3a9fa18a8d21
LoicStratil <stratil.loic@gmail.com>
2023-05-31 06:22:12 +0000

    visualize groundtruths/ ptrcnn/ ptpillar

--- autolabel_pipeline/visualize_pcds.py
+++ autolabel_pipeline/visualize_pcds.py
@@ -17,10 +17,15 @@ working_path = pathlib.Path(__file__).resolve().parents[1]
 FILE DESCRIPTION: 
 
 This file displays point clouds using open3d. The user can either specify a specific point cloud to be displayed or 
-randomly display 5 point clouds. The script loads the point clouds as well as the ground truth and point rcnn bboxs.
-Ground truths are displayed in red. Predicted boxes (point rcnn) in blue.  
-The script can be triggered directly. It loads arguments form autolabel.yaml (VISUALIZATION)
+randomly display 5 point clouds. The script can load ['groundtruths', 'pointrcnn', 'pointpillar']. The point cloud will
+always be displayed, bbox sources can be chosen in the .yaml file. 
+If one want to load only groundtruths --> BBOXES_TO_LOAD:['groundtruths'].
 
+Ground truths are displayed in red. 
+Predicted boxes (point rcnn) in blue.  
+Predicted boxes (point pillar) in green. 
+
+The script has to be triggered directly. It loads arguments form autolabel.yaml (VISUALIZATION)
 
 """
 
@@ -42,6 +47,45 @@ def load_config():
     return cfg
 
 
+# Function that generates an array containing only common elements present in every bbox source.
+def load_common_pcds(bbox_source):
+
+    bbox_source_elements = []
+
+    if not bbox_source:
+        print("Only pointcloud will be loaded, no bbox sources are selected.")
+
+    else:
+        for element in bbox_source:
+
+            if element == 'groundtruths':
+                groundtruth_file_names = get_file_names(cfg.DATA.PATH_GROUND_TRUTHS, '.txt')
+                bbox_source_elements.append(groundtruth_file_names)
+
+            if element == 'pointrcnn':
+                pointrcnn_file_names = get_file_names(cfg.DATA.PATH_PTRCNN_PREDICTIONS, '.csv')
+                bbox_source_elements.append(pointrcnn_file_names)
+
+            if element == 'pointpillar':
+                pointpillar_file_names = get_file_names(cfg.DATA.PATH_PTPILLAR_PREDICTIONS, '.csv')
+                bbox_source_elements.append(pointpillar_file_names)
+
+    # Load point clouds / Groundtruth data / Point-rcnn data:
+    point_cloud_files = get_file_names(cfg.DATA.PATH_POINT_CLOUDS, '.pcd')
+
+    # write all files that have all necessary information into common_elements_array.
+    if len(bbox_source_elements) > 0:
+        common_elements = set(bbox_source_elements[0])
+        for subarray in bbox_source_elements[1:]:
+            common_elements = common_elements.intersection(subarray)
+        common_elements_array = np.array(list(common_elements))
+
+    else:
+        common_elements_array = point_cloud_files
+
+    return common_elements_array
+
+
 # Function that loads the names of all files in a folder to an array
 def get_file_names(path_to_files, file_type):
 
@@ -54,7 +98,7 @@ def get_file_names(path_to_files, file_type):
 
 
 # Function that transforms predicted boxes to open3d boxes
-def translate_boxes_to_open3d_instance(gt_boxes, gt=False):
+def translate_boxes_to_open3d_instance(gt_boxes, gt):
     """
              4-------- 6
            /|         /|
@@ -69,33 +113,36 @@ def translate_boxes_to_open3d_instance(gt_boxes, gt=False):
     axis_angles = np.array([0, 0, gt_boxes[6] + 1e-10])
     rot = o3d.geometry.get_rotation_matrix_from_axis_angle(axis_angles)
     box3d = o3d.geometry.OrientedBoundingBox(center, rot, lwh)
-    if gt:
+    if gt == 'groundtruths':
         box3d.color = np.asarray([1, 0, 0])
-    else:
+
+    if gt == 'pointrcnn':
         box3d.color = np.asarray([0, 1, 1])
+
+    if gt == 'pointpillar':
+        box3d.color = np.asarray([0, 1, 0])
+
 
     line_set = o3d.geometry.LineSet.create_from_oriented_bounding_box(box3d)
     lines = np.asarray(line_set.lines)
     lines = np.concatenate([lines, np.array([[1, 4], [7, 6]])], axis=0)
     line_set.lines = o3d.utility.Vector2iVector(lines)
-    if gt:
+
+    if gt == 'groundtruths':
         line_set.paint_uniform_color(np.asarray([1, 0, 0]))
-    else:
+    if gt == 'pointrcnn':
         line_set.paint_uniform_color(np.asarray([0, 1, 1]))
+    if gt == 'pointpillar':
+        line_set.paint_uniform_color(np.asarray([0, 1, 0]))
 
     return box3d, line_set
 
 
 # This function loads the pcd, predicted boxes and ground-truths of a specified frame ID and visualizes it in open3d.
-def visualize(single_pcd):
+def visualize(single_pcd, bbox_source):
 
     print("Visualizing frame ID: ", single_pcd)
-    prcnn_file = os.path.join(cfg.VISUALISATION.PATH_PTRCNN_PREDICTIONS, single_pcd + ".csv")
-    gt_file = os.path.join(cfg.VISUALISATION.PATH_GROUND_TRUTHS, single_pcd + ".txt")
-    pcd_file = os.path.join(cfg.VISUALISATION.PATH_POINT_CLOUDS, single_pcd + ".pcd")
-    #print("Point-rcnn ID: ", prcnn_file)
-    #print("Ground truth ID: ", gt_file)
-    #print("Point cloud ID: ", pcd_file)
+    print("Sources: ", bbox_source)
 
     # initialize
     boxes_3d = []
@@ -110,74 +157,96 @@ def visualize(single_pcd):
     vis.add_geometry(mesh_frame)
 
     # point cloud
+    pcd_file = os.path.join(cfg.DATA.PATH_POINT_CLOUDS, single_pcd + ".pcd")
+    # print("Point cloud ID: ", pcd_file)
     pcd = o3d.io.read_point_cloud(pcd_file)
 
-    # Predicted bounding boxes POINT-RCNN
-    prcnn_pred_array = np.genfromtxt(prcnn_file, delimiter=',', dtype=str)
+    for element in bbox_source:
 
-    if prcnn_pred_array.ndim == 1:
-        prcnn_labels = prcnn_pred_array[0]
-        prcnn_boxes = prcnn_pred_array[1:].astype(float)
-        ret = translate_boxes_to_open3d_instance(prcnn_boxes, gt=False)
-        boxes_3d.append(ret[0])
-        line_sets.append(ret[1])
+        if element == 'groundtruths':
+            gt_file = os.path.join(cfg.DATA.PATH_GROUND_TRUTHS, single_pcd + ".txt")
 
-    else:
-        prcnn_labels = prcnn_pred_array[:, 0]
-        prcnn_boxes = prcnn_pred_array[:, 1:].astype(float)
+            # Ground truth bounding boxes
+            gt_array = np.genfromtxt(gt_file, delimiter=' ', dtype=str)
 
-        for box_idx, box in enumerate(prcnn_boxes[prcnn_boxes[:, 0].argsort()]):
-            # print(f"X,Y,Z,L,W,H,RotY,Score: {box}")
-            ret = translate_boxes_to_open3d_instance(box, gt=False)
-            boxes_3d.append(ret[0])
-            line_sets.append(ret[1])
+            if np.size(gt_array) == 0:
+                print("Ground-truths, ID ", my_pcd, ": ", "EMPTY ARRAY.")
+                continue
 
-    # Ground truth bounding boxes
-    gt_array = np.genfromtxt(gt_file, delimiter=' ', dtype=str)
+            if gt_array.ndim == 1:
+                gt_array = gt_array.reshape(1, -1)
 
-    if gt_array.ndim == 1:
-        gt_labels = gt_array[0]
-        gt_boxes = gt_array[1:].astype(float)
+            gt_labels = gt_array[:, 0]
+            gt_boxes = gt_array[:, 1:].astype(float)
+            gt_boxes = gt_boxes[gt_labels != 'DontCare']
+            # Convert groundtruth boxes to format: X,Y,Z,L,W,H,RotY
+            gt_boxes_reformatted = np.zeros((gt_boxes.shape[0], 7))
+            gt_boxes_reformatted[:, 0] = gt_boxes[:, 10]  # loc_x
+            gt_boxes_reformatted[:, 1] = gt_boxes[:, 11]  # loc_y
+            gt_boxes_reformatted[:, 2] = gt_boxes[:, 12]  # loc_z
+            gt_boxes_reformatted[:, 3] = gt_boxes[:, 9]  # dim_length
+            gt_boxes_reformatted[:, 4] = gt_boxes[:, 8]  # dim_width
+            gt_boxes_reformatted[:, 5] = gt_boxes[:, 7]  # dim_height
+            gt_boxes_reformatted[:, 6] = gt_boxes[:, 13]  # rot_y
 
-        if gt_labels[0] == 'DontCare':
-            pass
+            for box_idx, box in enumerate(gt_boxes_reformatted):
+                # box: X, Y, Z, L, W, H, Rot_Y
+                ret = translate_boxes_to_open3d_instance(box, gt='groundtruths')
+                boxes_3d.append(ret[0])
+                line_sets.append(ret[1])
 
-        # Convert groundtruth boxes to format: X,Y,Z,L,W,H,RotY
-        gt_boxes_reformatted = np.zeros(7)
-        gt_boxes_reformatted[0] = gt_boxes[10]  # loc_x
-        gt_boxes_reformatted[1] = gt_boxes[11]  # loc_y
-        gt_boxes_reformatted[2] = gt_boxes[12]  # loc_z
-        gt_boxes_reformatted[3] = gt_boxes[9]  # dim_length
-        gt_boxes_reformatted[4] = gt_boxes[8]  # dim_width
-        gt_boxes_reformatted[5] = gt_boxes[7]  # dim_height
-        gt_boxes_reformatted[6] = gt_boxes[13]  # rot_y
-        # box: X, Y, Z, L, W, H, Rot_Y
-        ret = translate_boxes_to_open3d_instance(gt_boxes_reformatted, gt=True)
-        boxes_3d.append(ret[0])
-        line_sets.append(ret[1])
+            print("Ground-truths, ID ", my_pcd, ": ", gt_labels)
+            # print("Ground truth ID: ", gt_file)
 
-    else:
-        gt_labels = gt_array[:, 0]
-        gt_boxes = gt_array[:, 1:].astype(float)
-        gt_boxes = gt_boxes[gt_labels != 'DontCare']
-        # Convert groundtruth boxes to format: X,Y,Z,L,W,H,RotY
-        gt_boxes_reformatted = np.zeros((gt_boxes.shape[0], 7))
-        gt_boxes_reformatted[:, 0] = gt_boxes[:, 10]  # loc_x
-        gt_boxes_reformatted[:, 1] = gt_boxes[:, 11]  # loc_y
-        gt_boxes_reformatted[:, 2] = gt_boxes[:, 12]  # loc_z
-        gt_boxes_reformatted[:, 3] = gt_boxes[:, 9]  # dim_length
-        gt_boxes_reformatted[:, 4] = gt_boxes[:, 8]  # dim_width
-        gt_boxes_reformatted[:, 5] = gt_boxes[:, 7]  # dim_height
-        gt_boxes_reformatted[:, 6] = gt_boxes[:, 13]  # rot_y
+        if element == 'pointrcnn':
+            prcnn_file = os.path.join(cfg.DATA.PATH_PTRCNN_PREDICTIONS, single_pcd + ".csv")
 
-        for box_idx, box in enumerate(gt_boxes_reformatted):
-            # box: X, Y, Z, L, W, H, Rot_Y
-            ret = translate_boxes_to_open3d_instance(box, gt=True)
-            boxes_3d.append(ret[0])
-            line_sets.append(ret[1])
+            # Predicted bounding boxes POINT-RCNN
+            prcnn_pred_array = np.genfromtxt(prcnn_file, delimiter=',', dtype=str)
 
-    print("Predicted pt-rcnn, ID ", my_pcd, ": ",  prcnn_labels)
-    print("Ground-truths, ID ", my_pcd, ": ", gt_labels)
+            if np.size(prcnn_pred_array) == 0:
+                print("Predicted pt-rcnn, ID ", my_pcd, ": ", "EMPTY ARRAY.")
+                continue
+
+            if prcnn_pred_array.ndim == 1:
+                prcnn_pred_array = prcnn_pred_array.reshape(1, -1)
+
+            prcnn_labels = prcnn_pred_array[:, 0]
+            prcnn_boxes = prcnn_pred_array[:, 1:].astype(float)
+
+            for box_idx, box in enumerate(prcnn_boxes[prcnn_boxes[:, 0].argsort()]):
+                # print(f"X,Y,Z,L,W,H,RotY,Score: {box}")
+                ret = translate_boxes_to_open3d_instance(box, gt='pointrcnn')
+                boxes_3d.append(ret[0])
+                line_sets.append(ret[1])
+
+            print("Predicted pt-rcnn, ID ", my_pcd, ": ", prcnn_labels)
+            # print("Pt-rcnn ID: ", prcnn_file)
+
+        if element == 'pointpillar':
+            ppillar_file = os.path.join(cfg.DATA.PATH_PTPILLAR_PREDICTIONS, single_pcd + ".csv")
+
+            # Predicted bounding boxes POINT-PILLAR
+            ppillar_pred_array = np.genfromtxt(ppillar_file, delimiter=',', dtype=str)
+
+            if np.size(ppillar_pred_array) == 0:
+                print("Predicted pt-pillar, ID ", my_pcd, ": ", "EMPTY ARRAY.")
+                continue
+
+            if ppillar_pred_array.ndim == 1:
+                ppillar_pred_array = ppillar_pred_array.reshape(1, -1)
+
+            ppillar_labels = ppillar_pred_array[:, 0]
+            ppillar_boxes = ppillar_pred_array[:, 1:].astype(float)
+
+            for box_idx, box in enumerate(ppillar_boxes[ppillar_boxes[:, 0].argsort()]):
+                # print(f"X,Y,Z,L,W,H,RotY,Score: {box}")
+                ret = translate_boxes_to_open3d_instance(box, gt='pointpillar')
+                boxes_3d.append(ret[0])
+                line_sets.append(ret[1])
+
+            print("Predicted pt-pillar, ID ", my_pcd, ": ", ppillar_labels)
+            # print("Pt-pillar ID: ", ppillar_file)
 
     vis.add_geometry(pcd)
     for box in boxes_3d:
@@ -194,6 +263,7 @@ def visualize(single_pcd):
     vis.update_renderer()
     vis.run()
     vis.destroy_window()
+    print("\n")
 
 
 if __name__== "__main__":
@@ -201,31 +271,25 @@ if __name__== "__main__":
     # Load EasyDict to access parameters.
     cfg = load_config()
 
-   # Load point clouds / Groundtruth data / Point-rcnn data:
-    point_cloud_files = get_file_names(cfg.VISUALISATION.PATH_POINT_CLOUDS, '.pcd')
-    groundtruth_file_names = get_file_names(cfg.VISUALISATION.PATH_GROUND_TRUTHS, '.txt')
-    pointrcnn_file_names = get_file_names(cfg.VISUALISATION.PATH_PTRCNN_PREDICTIONS, '.csv')
-    my_pcd = cfg.VISUALISATION.POINT_CLOUD_ID_TO_VISUALIZE
+    # define which bboxes to load for visualization.
+    bbox_source = cfg.VISUALISATION.BBOXES_TO_LOAD
+    pcds_common = load_common_pcds(bbox_source)
 
     # Check if a single file or multiple random files should be loaded.
+    my_pcd = cfg.VISUALISATION.POINT_CLOUD_ID_TO_VISUALIZE
+
     if my_pcd =='':
         # Load multiple random files.
-        pcds_common = []
-
-        for element in pointrcnn_file_names:
-            if element in point_cloud_files and element in groundtruth_file_names:
-                pcds_common.append(element)
-
         random.shuffle(pcds_common)
         pcds_selected = pcds_common[:5]
 
         for i in range (0, (len(pcds_selected))):
-            visualize(pcds_selected[i])
+            visualize(pcds_selected[i], bbox_source)
         print("done.")
 
     else:
         # Load single specified file.
-        if all(my_pcd in arr for arr in [point_cloud_files, groundtruth_file_names, pointrcnn_file_names]):
-            visualize(my_pcd)
+        if my_pcd in pcds_common:
+            visualize(my_pcd, bbox_source)
         else:
             raise ValueError('This point cloud ID is not available. Choose a different one.')
