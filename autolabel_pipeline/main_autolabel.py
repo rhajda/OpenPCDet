0fa5800c5adfff1e1cfc2d89e5bccafdcc00c4df
LoicStratil <stratil.loic@gmail.com>
2023-07-23 09:46:48 +0000

    pipeline main

--- autolabel_pipeline/main_autolabel.py
+++ autolabel_pipeline/main_autolabel.py
@@ -1,8 +1,10 @@
 
 # Import libraries
 from easydict import EasyDict
+from tqdm import tqdm
 import pandas as pd
 import numpy as np
+import warnings
 import pathlib
 import yaml
 import os
@@ -32,6 +34,14 @@ voting schemes:
 
 """
 
+BATCH_SIZE_VOTING = 10000
+
+# If not all frames should be voted on: Set START_AT_CHECKPOINT = True + set START_FRAME as first frame to be voted on.
+START_AT_CHECKPOINT = False
+START_FRAME = 100
+
+
+
 
 # Function that loads the YAML file to access parameters.
 def load_config():
@@ -48,6 +58,7 @@ def load_config():
 
     cfg = EasyDict(cfg_dict)
     return cfg
+
 # Function loads csv data of a specific frame to a dataframe containing all bboxes
 def csv_to_dataframe(path_data, file_name):
 
@@ -71,44 +82,133 @@ def csv_to_dataframe(path_data, file_name):
     df[df.columns[2:]] = df[df.columns[2:]].astype(float)
 
     return df
-# Function that loads the bboxes predicted for a frame ID to dataframe for every model.
-def load_frame_predictions(frame_ID, path_frames):
 
-    # sort all frames by ascending order for all models
-    all_frame_IDs = sorted(get_file_names(path_frames, '.csv'), key=lambda x: int(x))
+# Function that loads the bboxes predicted for a frame to dataframe for every model.
+def load_frame(frame_ID, path_frames):
+
     file_path = os.path.join(path_frames, frame_ID + ".csv")
-
-    # Load predictions for specific frame_ID  to dataframe
     df_frame_ID = csv_to_dataframe(file_path, frame_ID)
 
-    return all_frame_IDs, df_frame_ID
+    return df_frame_ID
+
+# Function that outputs the set of common and unique frames for n folders.
+def common_set_between_datasets(cfg, folders, FLAG_GENERATE_EMPTY_MISSING_FILES):
+    common_set = set()
+    unique_files = set()
+
+    for folder in folders:
+        files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+        file_set = set([os.path.splitext(f)[0] for f in files])
+        if common_set:
+            common_set = common_set.intersection(file_set)
+        else:
+            common_set = file_set
+        unique_files.update(file_set)
+        if cfg.PIPELINE.PRINT_INFORMATION:
+            print("Loaded: ", folder, "; Frames: ", len(files))
+
+    unique_files -= common_set
+    common_array = sorted(map(int, common_set))
+    uniques_array = sorted(map(int, unique_files))
+
+    if cfg.PIPELINE.PRINT_INFORMATION:
+        print("Common set origin: ", len(common_array))
+
+    if FLAG_GENERATE_EMPTY_MISSING_FILES:
+        if len(uniques_array) > 0:
+            # add empty csv files in the folder where no detections where predicted.
+            add_empty_prediction_files(uniques_array, folders)
+            if cfg.PIPELINE.PRINT_INFORMATION:
+                print("Empty frames added in corresponding folders for IDs: ", uniques_array)
+
+        combined_array = sorted(common_array + uniques_array)
+        if len(combined_array) == 0:
+            raise ValueError("No frames in the set.")
+
+        if cfg.PIPELINE.PRINT_INFORMATION:
+            print("Combined set: ", len(combined_array))
+
+        return combined_array
+
+    return common_array
+
+# Function that generated empty csv files to fill null predictions.
+def add_empty_prediction_files(unique_frames, folders):
+
+    for frame in unique_frames:
+        file_name = str(frame).zfill(6) + '.csv'
+        for folder in folders:
+            file_path = os.path.join(folder, file_name)
+            if not os.path.exists(file_path):
+                empty_array = np.empty((0,))
+                np.savetxt(file_path, empty_array, delimiter=',', fmt='%s')
+
+# Function that manages NMS or MAJORITY voting and feeds data to the voting schemes
+def vote_pseudo_labels(frame_set, batch_size_voting):
+
+    show_progress_bar = not cfg.PIPELINE.PRINT_INFORMATION
+
+    counter = 0
+    description = "Voting pseudo-labels (" + str(cfg.PIPELINE.VOTING_SCHEME) + ")"
+
+    for frame in tqdm(frame_set, desc=description, unit="frame", disable=not show_progress_bar):
+
+        frame_ID = str(frame).zfill(6)
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+
+            # Load all predicted frame IDs (ptrcnn & ptpillar & second)
+            df_ptrcnn = load_frame(frame_ID, cfg.DATA.PATH_PTRCNN_PREDICTIONS)
+            df_ptpillar = load_frame(frame_ID, cfg.DATA.PATH_PTPILLAR_PREDICTIONS)
+            df_second = load_frame(frame_ID, cfg.DATA.PATH_SECOND_PREDICTIONS)
+
+        if cfg.PIPELINE.PRINT_INFORMATION:
+            print("df_ptrcnn: ", "\n", df_ptrcnn)
+            print("df_ptpillar: ", "\n", df_ptpillar)
+            print("df_second: ", "\n", df_second)
+
+        if cfg.PIPELINE.VOTING_SCHEME == 'MAJORITY':
+            # majority_voting.
+            majority_voting.majority_voting(cfg, df_ptrcnn, df_ptpillar, df_second, frame_ID)
+
+        elif cfg.PIPELINE.VOTING_SCHEME == 'NMS':
+            # NMS voting
+            nms_voting.non_maximum_suppression_voting(cfg, df_ptrcnn, df_ptpillar, df_second, frame_ID)
+
+        else:
+            raise ValueError("No valid VOTING_SCHEME selected.")
+
+        if cfg.PIPELINE.SHOW_POINT_CLOUDS:
+            print("\n", "Visualization triggered:")
+            visualize_single_pcd(frame_ID, cfg.VISUALISATION.BBOXES_TO_LOAD, cfg)
+
+        counter +=1
+        if counter >= batch_size_voting:
+            manual_continue = input("Continue y/ n ? ")
+            if manual_continue == "y":
+                counter = 0
+                continue
+            else:
+                exit()
+
+
+def remove_smaller_numbers(numbers, value):
+    return [num for num in numbers if num >= value]
+
 
 
 if __name__ == "__main__":
 
-    # Choose element to display '000618',  '003658', '003676', '004769'
-    frame_ID = '000618'
-
     # Load EasyDict to access parameters.
     cfg = load_config()
+    frame_set = common_set_between_datasets(cfg,
+                                            [cfg.DATA.PATH_PTRCNN_PREDICTIONS,
+                                             cfg.DATA.PATH_PTPILLAR_PREDICTIONS,
+                                             cfg.DATA.PATH_SECOND_PREDICTIONS],
+                                            True)
 
-    # Load all predicted frame IDs (ptrcnn & ptpillar & second)
-    ptrcnn_all_frame_IDs, df_ptrcnn= load_frame_predictions(frame_ID, cfg.DATA.PATH_PTRCNN_PREDICTIONS)
-    ptpillar_all_frame_IDs, df_ptpillar = load_frame_predictions(frame_ID, cfg.DATA.PATH_PTPILLAR_PREDICTIONS)
-    second_all_frame_IDs, df_second = load_frame_predictions(frame_ID, cfg.DATA.PATH_SECOND_PREDICTIONS)
+    if START_AT_CHECKPOINT:
+        frame_set = remove_smaller_numbers(frame_set, START_FRAME)
 
-    if cfg.PIPELINE.PRINT_INFORMATION:
-        print("df_ptrcnn: ", "\n", df_ptrcnn)
-        print("df_ptpillar: ", "\n", df_ptpillar)
-        print("df_second: ", "\n", df_second)
-
-    # nms_voting.
-    #nms_voting.non_maximum_suppression_voting(cfg, df_ptrcnn, df_ptpillar, df_second, frame_ID)
-
-    # majority_voting.
-    majority_voting.majority_voting(cfg, df_ptrcnn, df_ptpillar, df_second, frame_ID)
-
-
-    if cfg.PIPELINE.SHOW_POINT_CLOUDS:
-        print("\n", "Visualization triggered:")
-        visualize_single_pcd(frame_ID, cfg.VISUALISATION.BBOXES_TO_LOAD, cfg)
+    vote_pseudo_labels(frame_set, BATCH_SIZE_VOTING)
