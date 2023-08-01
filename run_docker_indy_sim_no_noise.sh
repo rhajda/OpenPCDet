de0e42b26cd04143345916b31c0432c8b9fe8b32
Tobias Betz <t.betz@ftm.mw.tum.de>
2023-08-01 16:14:18 +0000

    adapted run files

--- run_docker_indy_sim_no_noise.sh
+++ run_docker_indy_sim_no_noise.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 gpu=$1
-docker run --rm -it --gpus "device=$gpu" --network=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/.Xauthority --hostname $(hostname) -v /home/calcstudenten/zulfaj_huch/data/for_training/sim_no_noise:/home/data/indy -v /data/huch/openpcdet_trainings/sim-to-real:/home/output openpcdet:indy113 bash
-
+docker run --rm -it --gpus "device=$gpu" --network=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/.Xauthority --hostname $(hostname) -v /home/ubuntu/Huch/data/Sim2RealDistributionAlignedDataset/sim:/home/data/indy -v /home/ubuntu/Huch/OpenPCDet_output:/home/output openpcdet:indy113 bash
