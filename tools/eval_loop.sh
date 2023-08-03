5c71d9d44e3e99f2936845ad4a1d5b649b58fb2f
Sebastian Huch <sebastian.huch@tum.de>
2023-08-03 17:07:49 +0200

    adapt eval_loop + run scripts to new datasets

--- tools/eval_loop.sh
+++ tools/eval_loop.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 NETWORK=pointrcnn # pointrcnn or pointpillar
-EVAL_TAG=sim_no_noise # real or sim_no_noise or sim_noise002 or sim_noise_down
-                        # EVAL_TAG = test dataset (same as mounted dataset in docker)
+EVAL_TAG=real # real or sim
+# EVAL_TAG = test dataset (same as mounted dataset in docker)
 
 # TRAIN_DATASET = train dataset
-for TRAIN_DATASET in default_real_run sim_no_noise sim_noise002 sim_noise_down
+for TRAIN_DATASET in real #sim sim2real real2sim
 do
-    # i = 5 training repetitions for each training dataset
-    for i in {1..5}
+    # i = 1 training repetitions for each training dataset
+    for i in {1..1}
         do
-                python test.py --cfg_file cfgs/indy_models/${NETWORK}.yaml --extra_tag "${TRAIN_DATASET}_${i}" --ckpt_dir /home/output/indy_models/${NETWORK}/${TRAIN_DATASET}_${i}/ckpt/ --eval_all --start_epoch 46 --eval_tag ${EVAL_TAG} --max_waiting_mins 0
+                python test.py --cfg_file cfgs/indy_models/${NETWORK}.yaml --extra_tag "${TRAIN_DATASET}" --ckpt_dir /home/output/indy_models/${NETWORK}/${TRAIN_DATASET}/ckpt/ --eval_all --start_epoch 70 --eval_tag ${EVAL_TAG} --max_waiting_mins 0
         done
 done
