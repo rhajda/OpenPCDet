0050fd25ee2cd1598beea17f48cf1feaa01140c7
Sebastian Huch <sebastian.huch@tum.de>
2023-08-14 10:41:56 +0200

    new dataset-agnostic run script

---
+++ run_docker_indy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gpu=$1
+docker run --rm -it --gpus "device=$gpu" --network=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/.Xauthority --hostname $(hostname) -v /home/ubuntu/Huch/data/Sim2RealDistributionAlignedDataset/:/home/data/indy -v /home/ubuntu/Huch/OpenPCDet_output:/home/output openpcdet:indy113 bash
+
