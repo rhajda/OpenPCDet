326fe517ae6b447d88a56d180c5f41ed46bb0265
Sebastian Huch <sebastian.huch@tum.de>
2023-08-14 12:45:18 +0200

    sim2real real2sim order

--- tools/eval_loop.sh
+++ tools/eval_loop.sh
@@ -2,9 +2,9 @@
 
 for NETWORK in pointrcnn pointpillar
 do
-  for TRAIN_DATASET in real sim sim2real real2sim
+  for TRAIN_DATASET in real sim real2sim sim2real
   do
-      for TEST_DATASET in real sim sim2real real2sim
+      for TEST_DATASET in real sim real2sim sim2real
           do
                   python test.py --cfg_file cfgs/indy_models/${NETWORK}.yaml --extra_tag "${TRAIN_DATASET}" --ckpt_dir /home/output/indy_models/${NETWORK}/${TRAIN_DATASET}/ckpt/ --eval_all --start_epoch 75 --dataset ${TEST_DATASET} --max_waiting_mins 0
           done
