de02a70129ea2a596f30d00ade6567cacd889324
Sebastian Huch <sebastian.huch@tum.de>
2023-08-18 14:58:54 +0200

    eval_loop default: start at epoch71

--- tools/eval_loop.sh
+++ tools/eval_loop.sh
@@ -6,7 +6,7 @@ do
   do
       for TEST_DATASET in real sim real2sim sim2real
           do
-                  python test.py --cfg_file cfgs/indy_models/${NETWORK}.yaml --extra_tag "${TRAIN_DATASET}" --ckpt_dir /home/output/indy_models/${NETWORK}/${TRAIN_DATASET}/ckpt/ --eval_all --start_epoch 75 --dataset ${TEST_DATASET} --max_waiting_mins 0
+                  python test.py --cfg_file cfgs/indy_models/${NETWORK}.yaml --extra_tag "${TRAIN_DATASET}" --ckpt_dir /home/output/indy_models/${NETWORK}/${TRAIN_DATASET}/ckpt/ --eval_all --start_epoch 71 --dataset ${TEST_DATASET} --max_waiting_mins 0
           done
   done
 done
