084e0cb344b75bb2bc9d15b2ef685380fdeea847
Sebastian Huch <sebastian.huch@tum.de>
2023-09-01 17:52:44 +0200

    also update forward_ret_dict during validation

--- pcdet/models/dense_heads/anchor_head_single.py
+++ pcdet/models/dense_heads/anchor_head_single.py
@@ -57,11 +57,10 @@ class AnchorHeadSingle(AnchorHeadTemplate):
         else:
             dir_cls_preds = None
 
-        if self.training:
-            targets_dict = self.assign_targets(
-                gt_boxes=data_dict['gt_boxes']
-            )
-            self.forward_ret_dict.update(targets_dict)
+        targets_dict = self.assign_targets(
+            gt_boxes=data_dict['gt_boxes']
+        )
+        self.forward_ret_dict.update(targets_dict)
 
         if not self.training or self.predict_boxes_when_training:
             batch_cls_preds, batch_box_preds = self.generate_predicted_boxes(
