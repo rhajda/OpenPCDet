a9792a3992108d3e60269131ec6b109e2201cc70
Sebastian Huch <sebastian.huch@tum.de>
2023-09-01 15:25:12 +0200

    train_loop and eval_loop with RUN loop

--- tools/eval_loop.sh
+++ tools/eval_loop.sh
@@ -4,9 +4,12 @@ for NETWORK in pointrcnn pointpillar
 do
   for TRAIN_DATASET in real sim real2sim sim2real
   do
+    for RUN in 1 2 3
+    do
       for TEST_DATASET in real sim real2sim sim2real
-          do
-                  python test.py --cfg_file cfgs/indy_models/${NETWORK}.yaml --extra_tag "${TRAIN_DATASET}" --ckpt_dir /home/output/indy_models/${NETWORK}/${TRAIN_DATASET}/ckpt/ --eval_all --start_epoch 71 --dataset ${TEST_DATASET} --max_waiting_mins 0
-          done
+      do
+        python test.py --cfg_file cfgs/indy_models/${NETWORK}.yaml --extra_tag "${TRAIN_DATASET}_${RUN}" --ckpt_dir /home/output/indy_models/${NETWORK}/${TRAIN_DATASET}_${RUN}/ckpt/ --eval_all --start_epoch 66 --dataset ${TEST_DATASET} --max_waiting_mins 0
+      done
+    done
   done
 done
--- tools/train_loop.sh
+++ tools/train_loop.sh
@@ -4,6 +4,9 @@ for NETWORK in pointrcnn pointpillar
 do
   for TRAIN_DATASET in real sim real2sim sim2real
   do
-    python train.py --cfg_file cfgs/indy_models/${NETWORK}.yaml --extra_tag "${TRAIN_DATASET}" --dataset "${TRAIN_DATASET}" --workers 16
+    for RUN in 1 2 3
+    do
+      python train.py --cfg_file cfgs/indy_models/${NETWORK}.yaml --extra_tag "${TRAIN_DATASET}_${RUN}" --dataset "${TRAIN_DATASET}" --workers 16
+    done
   done
 done
