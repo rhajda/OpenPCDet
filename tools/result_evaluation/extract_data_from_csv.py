234f0c9ccc51869a6b6988ce33ab2fe5dfa39ed6
Sebastian Huch <sebastian.huch@tum.de>
2023-09-01 18:48:28 +0200

    tsne plots

--- tools/result_evaluation/extract_data_from_csv.py
+++ tools/result_evaluation/extract_data_from_csv.py
@@ -1,13 +1,18 @@
 import os
 import csv
+import pickle
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches  # Import mpatches
+from sklearn.manifold import TSNE
 
 
 def extract_data():
+    # Initialize a dict to store max epochs
+    epochs = {}
+
     # Iterate over training datasets
     for training_dataset in training_datasets:
         # Iterate over evaluation ranges
@@ -27,7 +32,7 @@ def extract_data():
 
                     # Check if results.csv exists
                     if os.path.exists(results_csv_path):
-                        # Read the CSV file and extract the desired value
+                        # Read the CSV file and extract the desired value and epoch
                         with open(results_csv_path, "r") as csv_file:
                             csv_reader = csv.reader(csv_file)
 
@@ -35,28 +40,39 @@ def extract_data():
                             next(csv_reader)
 
                             values_list = []
+                            epochs_list = []
+
                             for row in csv_reader:
                                 epoch, values, _ = row
-                                values_list.append(float(values.split(",")[3]))  # Extract the desired value
+                                epoch = int(epoch)
+                                value = float(values.split(",")[3])  # Extract the desired value
 
-                                # Maintain a list with the last 'n' values
-                                if len(values_list) > last_n_epochs:
-                                    values_list.pop(0)
+                                # Append the value and epoch to the lists
+                                values_list.append(value)
+                                epochs_list.append(epoch)
 
-                            # Calculate the average of the last 'n' values
-                            average_value = sum(values_list) / len(values_list)
-                            max_value = max(values_list)
-                            last_value = values_list[-1]
-                            median_value = np.median(values_list)
-                            values = {"avg": average_value,
-                                      "max": max_value,
-                                      "last": last_value,
-                                      "median": median_value}
+                            # Calculate the average, last_value, max_value, and median_value based on last_n_epochs
+                            values_list_last_n = values_list[-last_n_epochs:]
+
+                            average_value = sum(values_list_last_n) / len(values_list_last_n)
+                            last_value = values_list_last_n[-1]
+                            max_value = max(values_list_last_n)
+                            median_value = np.median(values_list_last_n)
+
+                            values = {
+                                "avg": average_value,
+                                "max": max_value,
+                                "last": last_value,
+                                "median": median_value
+                            }
 
                             # Store the extracted value along with information about the dataset, eval range folder, and network
                             key = f"{network}_{training_dataset}_{eval_range_folder}"
                             extracted_data[key] = values[metric]
-                            extracted_data_list[key] = values_list
+                            extracted_data_list[key] = values_list_last_n  # Store the last_n_epochs values list
+
+                            # Store the epoch separately
+                            epochs[key] = max(epochs_list[-last_n_epochs:]) if metric == "max" else int(epoch)
 
     # Create a directory to store CSV files
     output_dir = "output_csv"
@@ -64,34 +80,102 @@ def extract_data():
 
     # Iterate over evaluation ranges
     for eval_range in evaluation_ranges:
-        # Create a CSV file for the current evaluation range
+        # Create a CSV file for the current evaluation range (extracted data)
         output_csv_path = os.path.join(output_dir, f"extracted_data_{eval_range}.csv")
 
-        # Open the CSV file for writing
-        with open(output_csv_path, "w", newline="") as csv_file:
+        # Create a CSV file for the current evaluation range (epochs)
+        epochs_output_csv_path = os.path.join(output_dir, f"epochs_{eval_range}.csv")
+
+        # Open the CSV files for writing
+        with open(output_csv_path, "w", newline="") as csv_file, open(epochs_output_csv_path, "w",
+                                                                      newline="") as epochs_csv_file:
             csv_writer = csv.writer(csv_file)
+            epochs_csv_writer = csv.writer(epochs_csv_file)
 
             # Write the data rows without the first column (training dataset)
             for training_dataset in training_datasets:
                 data_row = []
+                epochs_row = []  # Create a row for epochs
+
                 for eval_dataset in evaluation_datasets:
                     key = f"{network}_{training_dataset}_{eval_dataset}_{eval_range}"
                     data_row.append(extracted_data.get(key, ""))
+                    epoch = epochs.get(key, "")
+                    epochs_row.append(epoch)
+
+                # Write the rows to the respective CSV files
                 csv_writer.writerow(data_row)
+                epochs_csv_writer.writerow(epochs_row)
 
-    print(f"Extracted data saved to {output_dir}")
+    print(f"Extracted data and epoch data saved to {output_dir}")
 
 
-def plot_results():
-    # Define the specified bar colors
-    bar_colors = dict()
+# Define a function to extract data based on epoch information
+def extract_glob_feats():
+    # Initialize a dictionary to store extracted data
+    extracted_glob_feats = {}
+
+    # Define the output directory for CSVs
+    output_dir = "output_csv"
+
+    # Iterate over training datasets
+    for training_dataset in training_datasets:
+        # Iterate over evaluation ranges
+        for eval_range in evaluation_ranges:
+            # Iterate over evaluation datasets
+            for eval_dataset in evaluation_datasets:
+                # Construct the evaluation range folder name
+                eval_range_folder = f"{eval_dataset}_{eval_range}"
+
+                # Define the path to the epochs CSV file
+                epochs_csv_path = os.path.join(output_dir, f"epochs_{eval_range}.csv")
+
+                # Check if the epochs CSV file exists
+                if os.path.exists(epochs_csv_path):
+                    # Read the epochs CSV file
+                    with open(epochs_csv_path, "r") as epochs_csv_file:
+                        epochs_csv_reader = csv.reader(epochs_csv_file)
+
+                        for idx, row in enumerate(epochs_csv_reader):
+                            if idx == training_datasets.index(training_dataset):
+                                epoch = row[evaluation_datasets.index(eval_dataset)]  # Get the epoch for this dataset
+                                break
+
+                        # Define the folder for the current epoch
+                        epoch_folder = os.path.join(root_dir, network, training_dataset, "eval", "eval_all_default", eval_range_folder, f"epoch_{epoch}")
+
+                        # Check if the epoch folder exists
+                        if os.path.exists(epoch_folder):
+                            # Define the path to the pkl file
+                            pkl_file_path = os.path.join(epoch_folder, "test", f"glob_feat_{epoch}.pkl")
+
+                            # Check if the pkl file exists
+                            if os.path.exists(pkl_file_path):
+                                # Load data from the pkl file
+                                with open(pkl_file_path, "rb") as pkl_file:
+                                    data = pickle.load(pkl_file)
+
+                                # Create a key based on dataset, training_dataset, and eval_range
+                                key = f"{network}_{training_dataset}_{eval_range_folder}"
+
+                                extracted_glob_feats[key]= data
+
+    return extracted_glob_feats
+
+
+def define_colors():
+    # Define the specified colors
+    colors = dict()
     for i in range(num_real):
-        bar_colors[i] = tuple(np.asarray((190, 40 * i, 0)) / 255)
+        colors[i] = tuple(np.asarray((190, (255/num_real) * i / 2, 0)) / 255)
     for i in range(num_sim):
-        bar_colors[num_sim - i + num_real - 1] = tuple(np.asarray(((255/num_sim) * i, 202, 225)) / 255)
+        colors[num_sim - i + num_real - 1] = tuple(np.asarray(((255/num_sim) * i, 202, 225)) / 255)
     # sort dict by key
-    bar_colors = dict(sorted(bar_colors.items()))
+    colors = dict(sorted(colors.items()))
+    return colors
 
+
+def plot_results(colors):
     for eval_range in evaluation_ranges:
         # Load the data from the CSV file without headers (adjust the file path as needed)
         data = pd.read_csv(f"output_csv/extracted_data_{eval_range}.csv", header=None)
@@ -123,7 +207,7 @@ def plot_results():
                 df_transposed[training_dataset],
                 bar_width,
                 label=f'{training_dataset} (Training)',
-                color=bar_colors[i],  # Set the specified bar color
+                color=colors[i],  # Set the specified bar color
                 zorder = 2
             )
 
@@ -207,7 +291,7 @@ def plot_results():
             axs[i].set_ylim(0, 100)
 
             # Apply the same face colors as the bar plot
-            for patch, color in zip(boxplot['boxes'], bar_colors.values()):
+            for patch, color in zip(boxplot['boxes'], colors.values()):
                 patch.set_facecolor(color)
 
             # Add horizontal lines at every 10th value on the y-axis
@@ -226,7 +310,7 @@ def plot_results():
 
         # Create a custom legend for training datasets outside of the subplots
         legend_patches = [mpatches.Patch(color=color, label=training_dataset) for training_dataset, color in
-                          bar_colors.items()]
+                          colors.items()]
 
         # Include the evaluation range in the title of the figure
         fig.suptitle(f"Boxplots for Evaluation Range {eval_range} of last {last_n_epochs} epochs", fontsize=16)
@@ -242,7 +326,65 @@ def plot_results():
 
     # Show the plot (optional)
     plt.show()
-    print()
+
+
+def plot_tsne(extracted_glob_feats, evaluation_datasets, training_datasets, evaluation_ranges, colors, perplexity=30):
+    # Iterate over evaluation ranges
+    for eval_range in evaluation_ranges:
+        # Iterate over evaluation datasets
+        for eval_dataset in evaluation_datasets:
+            # Create a sub-plot for the current evaluation range and dataset
+            plt.figure(figsize=(12, 8))
+            plt.title(f"T-SNE Plot for Evaluation Range {eval_range} - Dataset {eval_dataset}")
+
+            # Create empty arrays to store t-SNE points and labels
+            tsne_points = np.array([])
+            tsne_labels = []
+
+            # Iterate over training datasets
+            for training_dataset in training_datasets:
+                # Define the key for extracted_glob_feats
+                key = f"{network}_{training_dataset}_{eval_dataset}_{eval_range}"
+
+                # Check if the key exists in extracted_glob_feats
+                if key in extracted_glob_feats:
+                    data = extracted_glob_feats[key]
+
+                    # Convert data to a NumPy array
+                    data = np.array(data)
+
+                    # Append the t-SNE points to the array
+                    if tsne_points.size == 0:
+                        tsne_points = data
+                    else:
+                        tsne_points = np.vstack((tsne_points, data))
+
+                    # Create labels for the current training dataset
+                    labels = np.full(data.shape[0], training_dataset)
+
+                    # Append the labels to tsne_labels
+                    tsne_labels.extend(labels)
+
+            # Use t-SNE to reduce the dimensionality to 2
+            tsne = TSNE(n_components=2, learning_rate='auto', init='pca', perplexity=perplexity, method="barnes_hut")
+            tsne_result = tsne.fit_transform(tsne_points)
+
+            # Scatter plot the t-SNE points with labels
+            unique_labels = np.unique(tsne_labels)
+
+            for i, label in enumerate(unique_labels):
+                x = tsne_result[np.array(tsne_labels) == label][:, 0]
+                y = tsne_result[np.array(tsne_labels) == label][:, 1]
+                scat = plt.scatter(x, y, color=colors[i], label=label)
+                scat.set_rasterized(True)
+
+            plt.xlabel("t-SNE Dimension 1")
+            plt.ylabel("t-SNE Dimension 2")
+            plt.legend()
+            plt.grid(True)
+
+    # Show or save the plot
+    plt.show()  # You can save the plot to a file using plt.savefig() if needed
 
 
 if __name__ == "__main__":
@@ -273,6 +415,16 @@ if __name__ == "__main__":
     # avg, max, last, median
     metric = "max"
 
+    ###########
+
     extract_data()
 
-    plot_results()
+    extracted_glob_feats = extract_glob_feats()
+
+    colors = define_colors()
+
+    # Plot bar charts and box plots
+    plot_results(colors)
+
+    # Call the function with your extracted_glob_feats, evaluation_datasets, training_datasets, evaluation_ranges, and optional perplexity
+    plot_tsne(extracted_glob_feats, evaluation_datasets, training_datasets, evaluation_ranges, colors, perplexity=15)
