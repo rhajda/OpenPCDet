03df29d157a0a92747f6b98f21bd497d1acb69dc
Sebastian Huch <sebastian.huch@tum.de>
2023-11-30 14:53:55 +0100

    rm old run scripts

--- run_docker_indy_sim2real.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gpu=$1
-docker run --rm -it --gpus "device=$gpu" --network=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/.Xauthority --hostname $(hostname) -v /home/ubuntu/Huch/data/Sim2RealDistributionAlignedDataset/sim2real:/home/data/indy -v /home/ubuntu/Huch/OpenPCDet_output:/home/output openpcdet:indy113 bash
-
--- run_docker_waymo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it --gpus '"device=1"' --network=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/.Xauthority --hostname $(hostname)  -v /home/calcmitarbeiter/huch/_Perception_Datasets/Waymo_Perception/:/home/data/waymo -v /data/huch/openpcdet_trainings/:/home/output openpcdet:latest bash
